d260f7caa1ee4c18d95c52029ab7cb8dc98f9df0
Rahul Jain <rahulj51@gmail.com>
2015-12-16 13:29:05 +0530

    Editing project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,6 +9,22 @@ class ProjectsController < ApplicationController
 		@project = Project.new
 	end
 
+	def update
+		@project = Project.find(params[:id])
+
+		if (@project.update(project_params))
+			flash[:notice] = "Project has been updated."
+			redirect_to @project
+		else
+			flash.now[:alert] = "Project has not been updated."
+			render "edit"
+		end
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
 	def create
 		@project = Project.new(project_params)
 
@@ -20,6 +36,7 @@ class ProjectsController < ApplicationController
 			render "new"
 		end
 	end
+
 
 	def show
 	  @project = Project.find(params[:id])
