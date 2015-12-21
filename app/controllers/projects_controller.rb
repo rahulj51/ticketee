4e24443727a56490bb31680af8ea2b6ab83b1b7a
Rahul Jain <rahulj51@gmail.com>
2015-12-21 16:12:02 +0530

    Deleting tickets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,12 +25,10 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		@project = Project.find(params[:id])
 		@project.destroy
 
 		flash[:notice] = "Project has been deleted."
 		redirect_to projects_path
-
 	end
 
 
