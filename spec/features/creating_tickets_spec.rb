2b2d603b9385bc56d520407c2e5715d046763b59
Rahul Jain <rahulj51@gmail.com>
2015-12-24 15:06:57 +0530

    Associating author with tickets

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -2,7 +2,10 @@ require "rails_helper"
 
 RSpec.feature "Users can create new tickets" do
 
+  let(:user) {FactoryGirl.create(:user)}
+
   before do
+    login_as(user)
     project = FactoryGirl.create(:project, name: "Internet Explorer")
 		visit project_path(project)
 		click_link "New Ticket"
@@ -11,10 +14,13 @@ RSpec.feature "Users can create new tickets" do
 	scenario "with valid attributes" do
 
 		fill_in "Name", with: "Non-standards compliance"
-		fill_in "Description", with: "My pages re ugly!"
+		fill_in "Description", with: "My pages are ugly!"
 		click_button "Create Ticket"
 
 		expect(page).to have_content "Ticket has been created."
+    within('#ticket') do
+      expect(page).to have_content "Author: #{user.email}"
+    end
 	end
 
 	scenario "with invalid attributes" do
